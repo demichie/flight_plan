1bd63db5f88061067f90de4e864f992c595df527
Mattia de' Michieli Vitturi <demichie@gmail.com>
2022-03-17 11:11:49 +0100

    added footer

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,90 @@ from folium import plugins
  
 st.set_page_config(layout="wide")
 
+import streamlit as st
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
+
+
+def image(src_as_string, **style):
+    return img(src=src_as_string, style=styles(**style))
+
+
+def link(link, text, **style):
+    return a(_href=link, _target="_blank", style=styles(**style))(text)
+
+
+def layout(*args):
+
+    style = """
+    <style>
+      # MainMenu {visibility: hidden;}
+      footer {visibility: hidden;}
+     .stApp { bottom: 61px; }
+    </style>
+    """
+
+    style_div = styles(
+        position="fixed",
+        left=0,
+        bottom=0,
+        margin=px(0, 0, 0, 0),
+        width=percent(100),
+        color="black",
+        text_align="center",
+        opacity=1
+    )
+
+    style_hr = styles(
+        display="block",
+        margin=px(8, 8, 5, 0),
+        border_style="inset",
+        border_width=px(2)
+    )
+
+    body = p()
+    foot = div(
+        style=style_div
+    )(
+        hr(
+            style=style_hr
+        ),
+        body
+    )
+    foot = div(
+        style=style_div
+    )(
+        hr(
+            style=style_hr
+        ),
+        body
+    )
+    st.markdown(style, unsafe_allow_html=True)
+
+    for arg in args:
+        if isinstance(arg, str):
+            body(arg)
+
+        elif isinstance(arg, HtmlElement):
+            body(arg)
+
+    st.markdown(str(foot), unsafe_allow_html=True)
+
+
+def footer():
+    myargs = [
+        "Made in ",
+        image('https://avatars3.githubusercontent.com/u/45109972?s=400&v=4',
+              width=px(25), height=px(25)),
+        " with ❤️ by ",
+        link("https://github.com/demichie", "@demichie"),
+        link("https://buymeacoffee.com/demichieW", image('https://i.imgur.com/thJhzOO.png')),
+    ]
+    layout(*myargs)
+
+
+
  
 step_to_home = 100 # m 
     
@@ -818,7 +902,8 @@ def main(array,csv_file,option,dx_perc_overlap,
     
 if __name__ == '__main__':
 
-   
+    footer()
+    
     m = folium.Map(location=[42.81233, 10.31297], zoom_start=12)
 
     # Add custom basemaps
